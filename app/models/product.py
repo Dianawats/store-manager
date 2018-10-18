748cc02ee2195656c0f65e77e44aa29ae1f9ed83
Dianawats <dianakiznaki@gmail.com>
2018-10-18 09:59:13 +0300

    update product model[Feature #161246437]

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,3 @@
-"model class for a product"
 from app.validation import Validation
 from flask import jsonify
 
@@ -6,26 +5,27 @@ all_products=[]
 validation = Validation()
 
 class Product:
+    """model class for a product"""
     def __init__(self, user_input):
         self.user_input = user_input
 
     def add_product(self):
-        #Add product item
+        """Method for adding product item"""
         data = self.user_input
-        search_keys = ("product_name", "quantity", "unit_price")
+        search_keys = ("product_name", "quantity", "price")
         if all(key in data.keys() for key in search_keys):
             
-            valid = validation.product_validation(data.get("product_name"), data.get("quantity"), data.get("unit_price"))
+            valid = validation.product_validation(data.get("product_name"), data.get("quantity"), data.get("price"))
             if valid:
                 return jsonify({"message":valid}), 400
             product = dict(
                 product_id = len(all_products) + 1,
                 product_name = data.get("product_name"),
                 quantity = data.get("quantity"),
-                unit_price = data.get("unit_price")
+                unit_price = data.get("price")
             )
             if any(d["product_name"] == data.get("product_name") for d in all_products):
-                return jsonify({"message":"product already exists, just update its quantity"}), 409       
+                return jsonify({"message":"product already exists, update its quantity"}), 409       
 
             all_products.append(product)
             return jsonify({"message":"product successfully added", "products":all_products}), 201
