a06bd3d7281d03872d436006b1c155b5eec3b4b4
Dianawats <dianakiznaki@gmail.com>
2018-10-18 10:32:17 +0300

    add single product route[Feature #161246491]

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import request
 from .models.product import Product
+from app.validation import Validation
 
 @app.route("/api/v1/products",methods=["POST"])
 """Add the product route"""
@@ -14,6 +15,16 @@ def add_product():
 def fetch_all_products():
     all_products = Product.fetch_all_products()
     return all_products 
+
+@app.route("/api/v1/products/<product_id>", methods=["GET"])
+"""route for a single product"""
+def fetch_single_product(product_id):
+    valid = validation.validate_input_type(product_id)
+    if valid:
+        return jsonify({"message":valid}), 400
+    product = Product(product_id)
+    single_product = product.fetch_single_product()
+    return single_product
 
 
 
