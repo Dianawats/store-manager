8e9ee385e81c6aeea1d6c9f676aa97bf2633f643
Dianawats <dianakiznaki@gmail.com>
2018-10-19 10:27:14 +0300

    add sales model[Feature #161246504]

---
+++ app/models/sales.py
@@ -0,0 +1,23 @@
+from app.models.product import Product
+from datetime import datetime
+
+
+product_obj = Product()
+
+class SaleRecord:
+    """Model class for a sales record"""
+    def __init__(self):
+        self.all_Sales = []
+
+    def create_sale_record(self, product, quantity, amount):
+        """This method creates a sales record"""
+        sale_record = dict(
+            sale_id = len(self.all_Sales)+1,
+            product = product,
+            quantity = quantity,
+            amount = amount,
+            attendant = "attendants_name",
+            date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        )
+        self.all_Sales.append(sale_record)
+        return True
