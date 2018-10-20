ff0891b681d12bce06e519e2007309684c29b1e2
Dianawats <dianakiznaki@gmail.com>
2018-10-20 09:51:02 +0300

    update models [Fixes #161246504]

--- app/models/product.py
+++ app/models/product.py
@@ -1,27 +1,28 @@
+"model class for a product"
+
 class Product(object):
-    """Product Class model"""
     def __init__(self):
         self.all_products = []
 
-    def add_product(self, product, quantity, price):
-        """This method adds product item"""
+    def add_product(self, product, quantity, unit_price):
+        #Add product item
         product = dict(
                 product_id = len(self.all_products) + 1,
                 product = product,
                 quantity = quantity,
-                price = price
+                unit_price = unit_price
             )
         self.all_products.append(product)
         return self.all_products
 
     def fetch_all_products(self):
-        """This method fetches all available products"""
+        # fetch all available products
         if len(self.all_products) > 0:
             return self.all_products
         return False
 
     def fetch_single_product(self, product_id):
-        """This method fetches a single product"""
+        # fetch a single product
         if len(self.all_products) > 0:
             for product in range(len(self.all_products)):
                 if ((self.all_products[product]["product_id"]) == int(product_id)):
