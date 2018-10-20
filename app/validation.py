61b14469150c06fd9268862221108be490614b1f
Dianawats <dianakiznaki@gmail.com>
2018-10-20 09:55:24 +0300

    update validation and add init for packaging [Fixes #161246461]

--- app/validation.py
+++ app/validation.py
@@ -1,38 +1,36 @@
 import re
 
 class Validation:
-    """validates product and return appropriate message"""
-
-    def product_validation(self, product_name, quantity, price):
+    # add product validation.
+    def product_validation(self, product_name, quantity, unit_price):
         if not product_name:
-            return "product_name is missing"
+            return "product name is missing"
         if product_name == " ":
-            return "product_name is missing"
-        if not re.match(r"^([a-zA-Z]+\s)*[a-zA-Z]+$", product_name):
-            return "product_name must have no white spaces"
+            return "product name is missing"
+        if not re.match(r"^([a-zA-Z]+[-_\s])*[a-zA-Z]+$", product_name):
+            return "product name must have no white spaces"
         if not re.match(r"^[0-9]*$", quantity):
             return "quantity must be only digits and must have no white spaces"
-        if not re.match(r"^[0-9]*$", price):
+        if not re.match(r"^[0-9]*$", unit_price):
             return "price must be only digits and must have no white spaces"    
         if len(product_name) < 4:
-            return "product_name should be more than 6 characters long"
+            return "product name should be more than 4 characters long"
         if not quantity:
             return "quantity is missing"
         if quantity == " ":
             return "quantity is missing"
-        if quantity < 1:
+        if int(quantity) < 1:
             return "quantity should be at least 1 item"    
-        if not price:
-            return "price is missing"
-        if price < 1:
-            return "price should be greater than zero"    
-        if price == " ":
-            return "price is missing"    
+        if not unit_price:
+            return "unit_price is missing"
+        if int(unit_price) < 1:
+            return "unit price should be greater than zero"    
+        if unit_price == " ":
+            return "unit_price is missing"    
         
     def validate_input_type(self, input):
         try:
             _input = int(input)
         except ValueError:
             return "Input should be an interger"
-
-    
+             
