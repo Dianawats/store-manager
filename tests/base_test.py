03ae590b3abc92a06ead27078ddfc8ab5177ee8e
Dianawats <dianakiznaki@gmail.com>
2018-10-23 02:29:33 +0300

    update tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -3,4 +3,7 @@ from run import app
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
+        """
+        This method executes before each test run
+        """
         self.app = app.test_client(self)
